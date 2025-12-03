f80b31157ce8669196bfffab1a067f2b3d4d649e
shubhamyadav-29 <shubhamarjun5@gmail.com>
2025-12-03 17:02:37 +0530

    new commit

--- myproject/mainapp/views.py
+++ myproject/mainapp/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
+from django.http import HttpResponse,JsonResponse
+
+def home(request):
+    data = {
+        "name":"Shubham",
+        "location":"mumbai"
+    }
+    return JsonResponse(data)
+
+
+
 # Create your views here.
