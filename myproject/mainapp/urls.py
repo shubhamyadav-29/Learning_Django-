e974735f1a34c242f1571bfdacf4c922b1af74e6
shubhamyadav-29 <shubhamarjun5@gmail.com>
2025-12-05 18:50:59 +0530

    commiting at Evening

--- myproject/mainapp/urls.py
+++ myproject/mainapp/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', views.homepage, name='homepage'),
+    path('todolist', views.todolist, name='home'),
+    path('contact', views.contact, name='home'),
+    path('about', views.about, name='home'),
 ]
--- myproject/mainapp/views.py
+++ myproject/mainapp/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
 
-def home(request):
+def homepage(request):
    return render(request, "main.html" )
+
+def todolist(request):
+   return render(request, "todolist.html" )
+
+def contact(request):
+   return render(request, "contact.html" )
+
+def about(request):
+   return render(request, "about.html" )
 
 
 
