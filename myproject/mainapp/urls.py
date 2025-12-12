21922951402032c0df9a50986c00d978d19409cd
shubhamyadav-29 <shubhamarjun5@gmail.com>
2025-12-12 16:50:54 +0530

    Done

--- myproject/mainapp/urls.py
+++ myproject/mainapp/urls.py
@@ -4,6 +4,11 @@ from . import views
 urlpatterns = [
     path('', views.homepage, name='homepage'),
     path('todolist/', views.todolist, name='todolist'),
+    path('todolist/delete_task/<task_id>', views.delete_task, name='delete_task'),
+    path('todolist/edit_task/<task_id>', views.edit_task, name='edit_task'),
+    path('todolist/complete/<task_id>', views.complete_task, name='complete_task'),
+    path('todolist/pending/<task_id>', views.pending_task, name='pending_task'),
     path('contact/', views.contact, name='contact'),
     path('about/', views.about, name='about'),
+   
 ]
--- myproject/mainapp/views.py
+++ myproject/mainapp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render , redirect
 from mainapp.models import Task
 from mainapp.forms import TaskForm
+from django.contrib import messages
 
 def homepage(request):
     context ={
@@ -14,8 +15,10 @@ def todolist(request):
       form_data = TaskForm(request.POST or None)
       if form_data.is_valid():
          form_data.save()
+         messages.success(request,"Task added Successfully.")
          return redirect("todolist")
-      
+      messages.success(request,"Something Went Wrong")
+       
    all_tasks = Task.objects.all()
    context = {
       'page':'TaskList',
@@ -35,5 +38,40 @@ def about(request):
    }
    return render(request, "about.html",context )
 
+def delete_task(request,task_id):
+   task_obj = Task.objects.get(id=task_id)
+   task_obj.delete()
+   messages.success(request,f"Task - {task_obj.task} deleted.")
+   return redirect("todolist")
+
+def edit_task(request,task_id):
+   task_obj = Task.objects.get(id=task_id)
+   if request.method == "POST":
+       form_data =TaskForm(request.POST or None,instance=task_obj)
+       if form_data.is_valid():
+         form_data.save()
+         messages.success(request,"Task Updated!")
+         return redirect("todolist")
+       messages.success(request,"Error Encounter in Task Updation!")
+   else:
+         context = {
+           'task_obj': task_obj
+       }
+   return render(request, "edit.html",context)
+   
+def complete_task(request,task_id):
+   task_obj = Task.objects.get(id= task_id)
+   task_obj.is_completed=True
+   task_obj.save()
+   messages.success(request,"Status Chnaged")
+   return redirect(todolist)
 
 
+def pending_task(request,task_id):
+   task_obj = Task.objects.get(id= task_id)
+   task_obj.is_completed=False
+   task_obj.save()
+   messages.success(request,"Status Chnaged")
+   return redirect(todolist)
+
+   
