e8088321fe8e13afa3093ff04b4a3b329675370c
shubhamyadav-29 <shubhamarjun5@gmail.com>
2025-12-14 12:59:09 +0530

    created the register page

--- myproject/users/views.py
+++ myproject/users/views.py
@@ -1,11 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from django.contrib.auth.forms import UserCreationForm
-
+from django.contrib import messages
 # Create your views here.
 
 def register(request):
-    register_form = UserCreationForm()
+    if request.method == "POST":
+        register_form = UserCreationForm(request.POST)
+        if register_form.is_valid():
+            register_form.save()
+            messages.success(request, "User Created successfully")
+            return redirect("todolist")
+    else:
+        register_form = UserCreationForm()
     return render(request,'register.html',{'register_form':register_form})
     
     
